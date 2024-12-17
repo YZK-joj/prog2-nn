68e48d7b98847645546a8b96148aaed433bf1568
yuhki fukuda <j3359vqq@sit.ac.jp>
2024-12-17 16:25:42 +0900

    途中２

--- train.py
+++ train.py
@@ -11,6 +11,8 @@ import torchvision.transforms.v2 as transforms
 import models
 
 
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
 ds_transform = transforms.Compose([
         transforms.ToImage(),
         transforms.ToDtype(torch.float32, scale=True)])
